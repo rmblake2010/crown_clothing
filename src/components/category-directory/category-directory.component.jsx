8c5d875f6e05ae242e1041cc939fd84ffc36af76
Ryan Blake <xbungalowx@gmail.com>
2022-12-05 18:26:08 -0600

    updating readme

---
+++ src/components/category-directory/category-directory.component.jsx
@@ -0,0 +1,17 @@
+import './category-directory.styles.scss'
+import CategoryItem from '../category-item/category-item.component'
+
+
+const CategoryDirectory = ({categories}) => {
+    return(
+        <div className='categories-container'>
+            {categories.map((category) => {
+                return (
+                    <CategoryItem key={category.id} category={category} />
+                )
+             })}
+        </div>
+    )
+}
+
+export default CategoryDirectory
